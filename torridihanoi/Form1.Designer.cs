713502282991cfa56098e54005e106cb1cc06468
unknown <10933991@itisrossi.vi.it>
2025-11-05 08:54:32 -0800

    finito

--- torridihanoi/Form1.Designer.cs
+++ torridihanoi/Form1.Designer.cs
@@ -28,12 +28,107 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            lbl_mosseMinime = new Label();
+            nud_dischi = new NumericUpDown();
+            btn_Avvia = new Button();
+            pnl_c1 = new Panel();
+            pnl_c2 = new Panel();
+            pnl_c3 = new Panel();
+            lbl_MosseEffettive = new Label();
+            ((System.ComponentModel.ISupportInitialize)nud_dischi).BeginInit();
+            SuspendLayout();
+            // 
+            // lbl_mosseMinime
+            // 
+            lbl_mosseMinime.AutoSize = true;
+            lbl_mosseMinime.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lbl_mosseMinime.Location = new Point(522, 23);
+            lbl_mosseMinime.Name = "lbl_mosseMinime";
+            lbl_mosseMinime.Size = new Size(257, 30);
+            lbl_mosseMinime.TabIndex = 0;
+            lbl_mosseMinime.Text = "Numero mosse stimate:  ";
+            // 
+            // nud_dischi
+            // 
+            nud_dischi.Location = new Point(147, 23);
+            nud_dischi.Maximum = new decimal(new int[] { 7, 0, 0, 0 });
+            nud_dischi.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+            nud_dischi.Name = "nud_dischi";
+            nud_dischi.Size = new Size(75, 23);
+            nud_dischi.TabIndex = 1;
+            nud_dischi.Value = new decimal(new int[] { 1, 0, 0, 0 });
+            // 
+            // btn_Avvia
+            // 
+            btn_Avvia.Location = new Point(38, 21);
+            btn_Avvia.Name = "btn_Avvia";
+            btn_Avvia.Size = new Size(75, 23);
+            btn_Avvia.TabIndex = 2;
+            btn_Avvia.Text = "Avvia";
+            btn_Avvia.UseVisualStyleBackColor = true;
+            btn_Avvia.Click += btn_Avvia_Click;
+            // 
+            // pnl_c1
+            // 
+            pnl_c1.BackColor = Color.SaddleBrown;
+            pnl_c1.Location = new Point(114, 297);
+            pnl_c1.Name = "pnl_c1";
+            pnl_c1.Size = new Size(35, 265);
+            pnl_c1.TabIndex = 3;
+            // 
+            // pnl_c2
+            // 
+            pnl_c2.BackColor = Color.SaddleBrown;
+            pnl_c2.Location = new Point(372, 297);
+            pnl_c2.Name = "pnl_c2";
+            pnl_c2.Size = new Size(35, 265);
+            pnl_c2.TabIndex = 4;
+            // 
+            // pnl_c3
+            // 
+            pnl_c3.BackColor = Color.SaddleBrown;
+            pnl_c3.Location = new Point(623, 297);
+            pnl_c3.Name = "pnl_c3";
+            pnl_c3.Size = new Size(35, 265);
+            pnl_c3.TabIndex = 4;
+            // 
+            // lbl_MosseEffettive
+            // 
+            lbl_MosseEffettive.AutoSize = true;
+            lbl_MosseEffettive.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lbl_MosseEffettive.Location = new Point(522, 66);
+            lbl_MosseEffettive.Name = "lbl_MosseEffettive";
+            lbl_MosseEffettive.Size = new Size(261, 30);
+            lbl_MosseEffettive.TabIndex = 5;
+            lbl_MosseEffettive.Text = "Numero mosse effettive: ";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(816, 609);
+            Controls.Add(lbl_MosseEffettive);
+            Controls.Add(pnl_c3);
+            Controls.Add(pnl_c2);
+            Controls.Add(pnl_c1);
+            Controls.Add(btn_Avvia);
+            Controls.Add(nud_dischi);
+            Controls.Add(lbl_mosseMinime);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)nud_dischi).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lbl_mosseMinime;
+        private NumericUpDown nud_dischi;
+        private Button btn_Avvia;
+        private Panel pnl_c1;
+        private Panel pnl_c2;
+        private Panel pnl_c3;
+        private Label lbl_MosseEffettive;
     }
 }
--- torridihanoi/Form1.cs
+++ torridihanoi/Form1.cs
@@ -1,51 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
 namespace TorriDiHanoi
 {
     public partial class Form1 : Form
     {
+        private System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
+        private List<(int from, int to)> mosse = new List<(int from, int to)>();
+        private int stepCorrente = 0;
+        private List<int>[] matrice;
+        private List<Panel> dischi = new List<Panel>();
+        int MosseAttuali;
+
         public Form1()
         {
             InitializeComponent();
+            timer.Interval = 600;
+            timer.Tick += Timer_Tick;
+            MosseAttuali = 0;
+        }
 
+        
+        static List<int>[] CreaMatrice(int n)
+        {
             List<int>[] matrice = new List<int>[]
             {
-                new List<int> { 1, 2, 3, 4, 5 },
+                new List<int>(),
                 new List<int>(),
                 new List<int>()
             };
-
-            int mosseTot = f(matrice, 0);
-            MessageBox.Show($"{mosseTot}");
+            for (int i = 1; i <= n; i++)
+            {
+                matrice[0].Add(i);
+            }
+            return matrice;
         }
 
-
-        static int f(List<int>[] matrice, int x)
+        static int f(List<int>[] matrice, int n, int inizio, int fine, List<(int from, int to)> mosse)
         {
-
-            int gSopra = matrice[x][0];
-
-            if (x >= 3)
+            if (n == 1)
             {
-                x = 0;
-            }
-            else x++;
-
-            int gNuovo;
-
-            if(matrice[x].Count == 0)
-            {
-                gNuovo = 0;
-            }else
-            {
-                gNuovo = matrice[x][0];//il nuovo � uguale al disco della colonna dopo alla posizione 0 (primo inserito)
-            }
-
-            if (gNuovo < gSopra)
-            {
+                mosse.Add((inizio, fine));
+                MoveDisk(matrice, inizio, fine);
                 return 1;
             }
-            matrice[x][matrice[x].Count] = gSopra;
+            else
+            {
+                int cAttuale = 3 - inizio - fine;
+                int mosseTot = f(matrice, n - 1, inizio, cAttuale, mosse);
+                mosse.Add((inizio, fine));
+                MoveDisk(matrice, inizio, fine);
+                mosseTot += 1 + f(matrice, n - 1, cAttuale, fine, mosse);
+                return mosseTot;
+            }
+        }
 
-            return f(matrice, x + 1);
+        static void MoveDisk(List<int>[] matrice, int inizio, int fine)
+        {
+            int discoTolto = matrice[inizio][matrice[inizio].Count - 1];
+            matrice[fine].Add(discoTolto);
+            matrice[inizio].RemoveAt(matrice[inizio].Count - 1);
+        }
+
+        private void btn_Avvia_Click(object sender, EventArgs e)
+        {
+            MosseAttuali = 0;
+            int n = (int)nud_dischi.Value;
+            dischi.ForEach(d => this.Controls.Remove(d));
+            dischi.Clear();
+            mosse.Clear();
+            stepCorrente = 0;
+
+            matrice = CreaMatrice(n);
+            int mosseTot = f(CreaMatrice(n), n, 0, 2, mosse);
+            lbl_mosseMinime.Text = $"Numero mosse stimate: {mosseTot}";
+
+            AggiungiDischiAColonna(n);
+            timer.Start();
+        }
+
+        private void AggiungiDischiAColonna(int n)
+        {
+            int altezzaDisco = 20;
+            int baseY = pnl_c1.Bottom - 5;
+
+            for (int i = 0; i < n; i++)
+            {
+                int valore = matrice[0][i];
+                int larghezza = 120 - (valore - 1) * 15; //disco più grande = numero più piccolo
+
+                Panel disco = new Panel
+                {
+                    Width = larghezza,
+                    Height = altezzaDisco,
+                    BackColor = Color.LightBlue,
+                    Left = pnl_c1.Left + (pnl_c1.Width - larghezza) / 2,
+                    Top = baseY - (i + 1) * altezzaDisco,
+                    BorderStyle = BorderStyle.FixedSingle,
+                    Tag = valore
+                };
+                this.Controls.Add(disco);
+                dischi.Add(disco);
+                disco.BringToFront();
+            }
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (stepCorrente < mosse.Count)
+            {
+                var step = mosse[stepCorrente++];
+                EseguiMossa(step.from, step.to);
+            }
+            else
+            {
+                timer.Stop();
+            }
+        }
+
+        private void EseguiMossa(int from, int to)
+        {
+            if (matrice[from].Count == 0) return;
+
+            int discoDaSpostare = matrice[from][matrice[from].Count - 1];
+            Panel disco = dischi.Find(d => (int)d.Tag == discoDaSpostare);
+
+            MoveDisk(matrice, from, to);
+            AggiornaPosizioneDisco(disco, to);
+        }
+
+        private void AggiornaPosizioneDisco(Panel disco, int colonna)
+        {
+            int nDischiInColonna = matrice[colonna].Count;
+            int altezzaDisco = disco.Height;
+            int baseY = pnl_c1.Bottom - 5;
+
+            int nuovaY = baseY - nDischiInColonna * altezzaDisco;
+
+            int nuovaX = 0;
+            if (colonna == 0) nuovaX = pnl_c1.Left + (pnl_c1.Width - disco.Width) / 2;
+            if (colonna == 1) nuovaX = pnl_c2.Left + (pnl_c2.Width - disco.Width) / 2;
+            if (colonna == 2) nuovaX = pnl_c3.Left + (pnl_c3.Width - disco.Width) / 2;
+
+            
+            int passi = 10;
+            int dx = (nuovaX - disco.Left) / passi;
+            int dy = (nuovaY - disco.Top) / passi;
+
+            for (int i = 0; i < passi; i++)
+            {
+                disco.Left += dx;
+                disco.Top += dy;
+                Application.DoEvents();
+                System.Threading.Thread.Sleep(20);
+            }
+
+            disco.Left = nuovaX;
+            disco.Top = nuovaY;
+            MosseAttuali++;
+            lbl_MosseEffettive.Text = $"Numero mosse effettive: {MosseAttuali}";
+
         }
     }
 }
